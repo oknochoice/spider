f3f55f22792d54ec2415da768ceddaae0b54a9c4
Jerry <oknochoice@gmail.com>
2018-09-12 20:58:27 +0800

    practice dir

--- buildingScrapers.py
+++
@@ -1,9 +0,0 @@
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
-
-html = urlopen('http://pythonscraping.com/pages/page1.html')
-bs = BeautifulSoup(html.read(), 'html5lib')
-print(bs.h1)
-
-
-
